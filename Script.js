25c19596be4edff45ac82b1816dca43bf889838b
IDatsuk <johnny142usa@gmail.com>
2024-09-28 14:18:13 +0700

    Add mobile navigation

--- Script.js
+++ Script.js
@@ -4,3 +4,15 @@ function checkEmail() {
     else if(!email.includes('.')) alert('Нет символа .');
     else alert('Все отлично!');
 }
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = 'Images/nav/nav-close.svg';
+    } else {
+        navBtnImg.src ='Images/nav/nav-open.svg';
+    }
+}
